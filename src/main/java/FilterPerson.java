fc49b803e0863dfa3387c4bc69ea096ffb0e6892
Krystalone7 <kristallix2002@MAIL.RU>
2021-11-02 20:24:30 +0400

    v1

--- src/main/java/FilterPerson.java
+++ src/main/java/FilterPerson.java
@@ -1,2 +1,36 @@
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class FilterPerson {
+
+    public static List<Person> filter(List<Person> persons, Map<String, String> criteria){
+        for (String criterion: criteria.keySet()){
+            switch (criterion) {
+                case "name":
+                    persons = persons.stream()
+                            .filter(person -> person.getName().equals(criteria.get(criterion)))
+                            .collect(Collectors.toList());
+                    break;
+                case "surname":
+                    persons = persons.stream()
+                            .filter(person -> person.getSurname().equals(criteria.get(criterion)))
+                            .collect(Collectors.toList());
+                    break;
+                case "date":
+                    persons = persons.stream()
+                            .filter(person -> person.getDate().equals(LocalDate.parse(criteria.get(criterion))))
+                            .collect(Collectors.toList());
+                    break;
+                case "gender":
+                    persons = persons.stream()
+                            .filter(person -> person.getGender().equals(Gender.valueOf(criteria.get(criterion))))
+                            .collect(Collectors.toList());
+                    break;
+            }
+        }
+        return persons;
+    }
+
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -3,9 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,17 +20,44 @@ class PersonTest {
         persons.add(new Person("Nikita", "Sidorov", LocalDate.of(2002, 8, 5), Gender.MALE));
         persons.add(new Person("Elisey", "Kuznetzov", LocalDate.of(2002, 3, 8), Gender.MALE));
         persons.add(new Person("Jenya", "Alekseev", LocalDate.of(2002, 12, 12), Gender.MALE));
+        persons.add(new Person("Jenya", "Alekseev", LocalDate.of(2013, 4, 5), Gender.MALE));
         persons.add(new Person("Slava", "Lomakin", LocalDate.of(2002, 11, 15), Gender.MALE));
         persons.add(new Person("Anna", "Volkova", LocalDate.of(2002, 7, 6), Gender.FEMALE));
         persons.add(new Person("Julia", "Orlova", LocalDate.of(2002, 4, 5), Gender.FEMALE));
         persons.add(new Person("Daria", "Simonova", LocalDate.of(2002, 5, 7), Gender.FEMALE));
-        for (Person person: persons){
-            System.out.println(person);
-        }
+        //for (Person person: persons){
+        //    System.out.println(person);
+        //}
     }
 
     @Test
     public void filterByNameArtem(){
+        Map<String, String> criteria = new HashMap<>();
+        criteria.put("name", "Artem");
+        List<Person> filteredPersons = FilterPerson.filter(persons, criteria);
+        List<Person> expected = new ArrayList<>();
+        expected.add(new Person("Artem", "Brown", LocalDate.of(2012, 1, 25), Gender.MALE));
+        expected.add(new Person("Artem", "Ivanov", LocalDate.of(2002, 11, 7), Gender.MALE));
+        System.out.println(expected);
+        System.out.println(filteredPersons);
+        assertIterableEquals(expected, filteredPersons);
+    }
+
+    @Test
+    public void filterBySurnameIvanov(){
+        Map<String, String> criteria = new HashMap<>();
+        criteria.put("surname", "Ivanov");
+        List<Person> filteredPersons = FilterPerson.filter(persons, criteria);
+        List<Person> expected = new ArrayList<>();
+        expected.add(new Person("Petr", "Ivanov", LocalDate.of(2002, 11, 20), Gender.MALE));
+        expected.add(new Person("Artem", "Ivanov", LocalDate.of(2002, 11, 7), Gender.MALE));
+        System.out.println(expected);
+        System.out.println(filteredPersons);
+        assertIterableEquals(expected, filteredPersons);
+    }
+
+    @Test
+    public void filterByNameJenyaAndSurnameAlekseev(){
 
     }
 
